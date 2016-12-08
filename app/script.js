b86211dd05ba1ac70ab483ce3f2dc624b4817199
Seth Sutherland <cokolatepal@gmail.com>
2016-12-08 14:37:26 -0700

    created some functions

--- app/script.js
+++ app/script.js
@@ -1,5 +1,23 @@
 $('document').ready(function(){
 
+    var advanceTask = function(task) {
+        var modified = task.innerText.trim()
+        for (var i = 0; i < listo.length; i++) {
+            if (listo[i].task === modified) {
+                if (listo[i].id === 'new') {
+                    listo[i].id = 'inProgress';
+                } else if (listo[i].id === 'inProgress') {
+                    listo[i].id = 'archived';
+                } else {
+                    listo.splice(i, 1);
+                }
+                break;
+            }
+        }
+        task.remove();
+    };
+
+
     var listo = [];
     var Task = function(task) {
         this.task = task;
