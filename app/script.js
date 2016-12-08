f9e99c25d4abfe8b692c5a56c90a5091adb94173
Seth Sutherland <cokolatepal@gmail.com>
2016-12-08 14:07:48 -0700

    after about half the changes I need to make

--- app/script.js
+++ app/script.js
@@ -6,6 +6,29 @@ $('document').ready(function(){
         this.id = "new";
     }
 
+    $('#newTaskForm').hide();
+
+
+
+    var addTask = function(task){
+        if(task){
+            task = new Task(task);
+            listo.push(task);
+            $('#newItemInput').val('');
+            $('#newList').append(
+                '<a href="#finish" class="" id="item">'  +
+                    '<li class="list-group-item">'  +
+                    '<h3>' +task.task + '</h3>>'  +
+                    '<span class="glyphicon glyphicon-arrow-right">'  +
+                    '</span>' +
+                    '</li>'  +
+                    '</a>'
+            );
+        }
+        $('#newTaskForm').slideToggle('fast', 'linear');
+    };
+
+
 
 
 
